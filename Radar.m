4b76476797d16d947c32a528514167dd15ce503c
Josep Dols <jodoldar@gmail.com>
2017-06-20 18:45:42 +0200

    Image import
    Añadido codigo para importar la imagen y hacer un preprocesado rápido, previo a la extraccion de datos

---
+++ Radar.m
@@ -0,0 +1,28 @@
+tic
+[imagen,color]=imread('200620171710.gif');
+pos=1;
+colors = zeros(1,11);
+for i=146:24:386
+    colors(pos)=imagen(508,i);
+    pos = pos+1;
+end
+todos = 1:1:length(color);
+sobra = setdiff(todos,colors);
+imagenRes = imagen;
+for j=unique(sobra)
+    imagenRes(imagen==j)=0;
+end
+image(imagenRes);
+colormap(color);
+%pause(2);
+imagenRes = imcrop(imagenRes,[0 0 485 480]);
+image(imagenRes);
+colormap(color);
+pause(2);
+
+[Al,An] = size(imagenRes);
+for k=1:1:length(colors)
+    image(imagenRes==colors(k));
+    pause(0.5);
+end
+toc
