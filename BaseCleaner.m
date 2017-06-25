ea78b32dd3b13d258e228b69026106ec042ab667
Josep Dols <jodoldar@gmail.com>
2017-06-25 12:27:26 +0200

    Ya se tiene la base de las provincias

--- BaseCleaner.m
+++ BaseCleaner.m
@@ -8,7 +8,8 @@ for i=2:1:numImages
     currentImg = imread(strcat(imgNames(i).folder,'\',imgNames(i).name));
     currentImg = imcrop(currentImg,[0 0 485 480]);
     carriedBase = carriedBase&currentImg;
-    imshow(carriedBase);
+    %imshow(carriedBase);
     %pause(0.5);
 end
+save('BaseProvincias','carriedBase');
 toc
